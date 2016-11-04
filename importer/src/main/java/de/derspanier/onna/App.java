337264735264bc65e760779ead423d3567d91e94
KonradENF <konrad.fahrendholz@outlook.com>
2016-11-04 15:25:27 +0100

    Initial commit

---
+++ importer/src/main/java/de/derspanier/onna/App.java
@@ -0,0 +1,13 @@
+package de.derspanier.onna;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
